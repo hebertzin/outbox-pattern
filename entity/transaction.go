2b5e4954c4e8f37e9427ffbcc98d6082c7690322
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-14 13:40:07 -0300

    feat: create transaction entity

---
+++ entity/transaction.go
@@ -0,0 +1,20 @@
+package entity
+type TransactionStatus string
+
+const (
+	StatusPending    TransactionStatus = "PENDING"
+	StatusProcessing TransactionStatus = "PROCESSING"
+	StatusCompleted  TransactionStatus = "COMPLETED"
+	StatusFailed     TransactionStatus = "FAILED"
+)
+
+type Transaction struct {
+	Id string `json:"id"`
+	Amount int64 `json:"amount"`
+	Description string `json:"description"`
+	FromUserId string `json:"fromUserId"`
+	ToUserId string `json:"toUserId"`
+	TransactionStatus TransactionStatus `json:"transactionStatus"`
+	CreatedAt string `json:"createdAt"`
+	ProcessedAt string `json:"processedAt"`
+}
