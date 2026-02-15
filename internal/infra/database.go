f4082e12928259aa5f10d4724fad9186b098d99f
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-15 11:53:59 -0300

    feat: create a method to connect in database

--- internal/infra/database.go
+++ internal/infra/database.go
@@ -1,1 +1,48 @@
 package infra
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+var (
+	MaxConnectionsOpen = 25
+	DriverName         = "postgres"
+)
+
+type DatabaseConnection struct {
+	DB *sql.DB
+}
+
+func NewDatabaseConnection(
+	host string,
+	port int,
+	user string,
+	password string,
+	dbname string,
+) (*DatabaseConnection, error) {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname,
+	)
+
+	db, err := sql.Open(DriverName, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(MaxConnectionsOpen)
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &DatabaseConnection{
+		DB: db,
+	}, nil
+}
