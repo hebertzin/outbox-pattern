4e7553e062a4b097ef1d185966876a3d4851b529
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-23 23:56:47 -0300

    feat: starting implement handler producer

--- internal/core/httphandler/create_transaction.go
+++ internal/core/httphandler/create_transaction.go
@@ -1,1 +1,30 @@
 package httphandler
+
+import (
+	"context"
+	"encoding/json"
+	"transaction-service/internal/core/broker"
+)
+
+func NewCreateTransaction(b *broker.RabbitMQ) *Handler {
+	pub := broker.NewPublisher(b, "created.transaction", "")
+
+	return &Handler{
+		pub: pub,
+	}
+}
+
+type Handler struct {
+	pub *broker.Publisher
+}
+
+func (handler *Handler) handle() {
+	data, err := json.Marshal(struct {
+		Test string
+	}{})
+	if err != nil {
+		panic(err)
+	}
+
+	handler.pub.Publish(context.Background(), data)
+}
